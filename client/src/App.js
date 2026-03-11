f9e358d6b1d1948ba6f44b8326b785d8ce563534
Rajkumar Mali <rajkumarmali@192.168.0.101>
2026-03-11 20:24:16 +0530

    Add the Product details

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Navigation from "./customer/components/navigation/Navigation";
 import Product from "./customer/components/product/Product";
+import ProductDetails from "./customer/components/productDetails/ProductDetails";
 import HomePage from "./customer/pages/homePage/HomePage";
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
       <Navigation />
       <div>
         {/* <HomePage /> */}
-        <Product />
+        {/* <Product /> */}
+        <ProductDetails />
       </div>
     </div>
   );
