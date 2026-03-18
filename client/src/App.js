7ba3649749a2ef7173fec8217f1c3ffabbb33243
Rajkumar Mali <rajkumarmali@192.168.0.101>
2026-03-18 20:42:42 +0530

    Order History and Order Details

--- client/src/App.js
+++ client/src/App.js
@@ -3,20 +3,21 @@ import './App.css';
 import CheckOut from './customer/components/checkOut/CheckOut';
 import Cart from './customer/components/cart/Cart';
 import Navigation from './customer/components/navigation/Navigation';
+import Order from './customer/components/order/Order';
 import HomePage from './customer/pages/homePage/HomePage';
 import { BrowserRouter } from "react-router-dom";
+import OrderDetails from './customer/components/order/OrderDetails';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Navigation />
-        <div>
-          {/* <HomePage /> */}
-          <CheckOut />
-        </div>
+    <div className="App">
+      <Navigation />
+      <div>
+        {/* <HomePage /> */}
+        {/* <Order /> */}
+        <OrderDetails />
       </div>
-    </BrowserRouter>
+    </div>
   );
 }
 
