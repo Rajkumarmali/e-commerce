aa8a15f8500aee37c0daae5192ce2b6b5788ffe3
Rajkumar  Saini <rajkumarmali@Rajkumars.local>
2026-05-11 14:18:39 +0530

    add config

---
+++ server /src/main/java/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package config;
+
+public class JwtConstant {
+    public static final String SECRATE_KEY="qwertyuiopasdfghjklzxcvbnm";
+    public static final String JWT_HEADER="Authorization";
+}
