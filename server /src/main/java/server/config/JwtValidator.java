f0464ccb74345f2d211ecd213bc9114423e5e74b
Rajkumar  Saini <rajkumarmali@Rajkumars.local>
2026-05-20 01:19:33 +0530

    fix the issue

--- server /src/main/java/server/config/JwtValidator.java
+++ server /src/main/java/server/config/JwtValidator.java
@@ -7,22 +7,20 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import javax.crypto.SecretKey;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.OncePerRequestFilter;
 
-import javax.crypto.SecretKey;
-import java.io.IOException;
-import java.util.List;
 
 public class JwtValidator extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-
         String path = request.getServletPath();
         if(path.equals("/auth/signin") || path.equals("/auth/signup")) {
             filterChain.doFilter(request, response);
