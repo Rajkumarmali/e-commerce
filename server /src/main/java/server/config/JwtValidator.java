f9356668e940edf7ec81c889fbbb0cbe006381e7
Rajkumar  Saini <rajkumarmali@Rajkumars.local>
2026-05-20 12:09:28 +0530

    .

--- server /src/main/java/server/config/JwtValidator.java
+++ server /src/main/java/server/config/JwtValidator.java
@@ -16,6 +16,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 
 public class JwtValidator extends OncePerRequestFilter {
